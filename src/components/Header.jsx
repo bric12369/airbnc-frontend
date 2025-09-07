ba856d6bf1a99c90686dcc09239fa28fe282ac03
bric12369 <barryrichards05@gmail.com>
2025-09-07 14:03:23 +0100

    updated filepaths

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@
 const Header = () => {
 
     return <div id='header'>
-        <img src="../../public/home.png" alt="home-icon" className="header-icon" />
+        <img src="/home.png" alt="home-icon" className="header-icon" />
         <h1>AirBnC</h1>
-        <img src="../../public/avatar.png" alt="profile-icon" className="header-icon" />
+        <img src="/avatar.png" alt="profile-icon" className="header-icon" />
     </div>
 }
 

069292e1e38ce6c4011de530f00ed75a18e57096
bric12369 <barryrichards05@gmail.com>
2025-09-07 13:59:30 +0100

    made Header component with basic styling

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+const Header = () => {
+
+    return <div id='header'>
+        <img src="../../public/home.png" alt="home-icon" className="header-icon" />
+        <h1>AirBnC</h1>
+        <img src="../../public/avatar.png" alt="profile-icon" className="header-icon" />
+    </div>
+}
+
+export default Header

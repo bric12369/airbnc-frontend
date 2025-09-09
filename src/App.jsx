7ed16a81eac6f0e59fb31f8c5bd85ca34b7f0cfe
bric12369 <barryrichards05@gmail.com>
2025-09-09 21:02:52 +0100

    created user context

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,23 @@ import { useState } from 'react'
 import Header from './components/Header'
 import HomePage from './components/HomePage'
 import PropertyDetailsPage from './components/PropertyDetailsPage'
+import UserContext from './Contexts/UserContext'
 
 function App() {
-const [properties, setProperties] = useState([])
+  const [properties, setProperties] = useState([])
 
   return (
-    <>
-      <Header />
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage properties={properties} setProperties={setProperties}/>} />
-          <Route path='/properties/:id' element={<PropertyDetailsPage />} />
-        </Routes>
-      </BrowserRouter>
-    </>
+    <UserContext>
+      <>
+        <Header />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<HomePage properties={properties} setProperties={setProperties} />} />
+            <Route path='/properties/:id' element={<PropertyDetailsPage />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    </UserContext>
   )
 }
 

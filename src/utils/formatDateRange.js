8bd8369acedc51afca1dca850ac507026c04f144
bric12369 <barryrichards05@gmail.com>
2025-09-10 18:02:14 +0100

    updated hours to tackle timezone issues

--- src/utils/formatDateRange.js
+++ src/utils/formatDateRange.js
@@ -1,5 +1,4 @@
 
-
 export function convertDateISOtoString(ISODate) {
 
     return new Date(ISODate).toDateString()
@@ -11,7 +10,9 @@ export function getDateRange(startISODate, endISODate) {
     const allDates = []
 
     const startDate = new Date(startISODate)
+    startDate.setHours(12, 0, 0, 0)
     const endDate = new Date(endISODate)
+    endDate.setHours(12, 0, 0, 0)
     let currentDate = new Date(startDate)
 
     while (currentDate <= endDate) {

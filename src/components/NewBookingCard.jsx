617ee60ee1738954ddd6074388ea5ead006cf7a3
bric12369 <barryrichards05@gmail.com>
2025-09-10 18:44:20 +0100

    separated into custom hook

--- src/components/NewBookingCard.jsx
+++ src/components/NewBookingCard.jsx
@@ -1,54 +1,11 @@
 import Calendar from 'react-calendar'
 import 'react-calendar/dist/Calendar.css';
-import { useContext, useEffect, useMemo, useState } from 'react';
-import { useParams } from 'react-router';
-import { fetchPropertyBookings, postBooking } from '../utils/apiCalls';
-import UserContext from '../Contexts/UserContext';
-import { formatDate, getDateRange } from '../utils/formatDates';
+import useBooking from '../hooks/useBooking';
 
 
 const NewBookingCard = () => {
-    const [newBooking, setNewBooking] = useState({})
-    const [currBookings, setCurrBookings] = useState([])
 
-    const { id } = useParams()
-
-    const user = useContext(UserContext)
-
-    useEffect(() => {
-        fetchPropertyBookings(id).then((response) => {
-            setCurrBookings(response.data.bookings)
-        })
-    }, [id])
-
-    let bookedDates = useMemo(() => {
-
-        const dates = []
-
-        for (const booking of currBookings) {
-            dates.push(getDateRange(booking.check_in_date, booking.check_out_date))
-        }
-        return dates.flat()
-
-    }, [currBookings])
-
-    const handleChange = (range) => {
-        const [start, end] = range
-        console.log(start)
-
-        setNewBooking({
-            'guest_id': user === undefined ? 1 : user,
-            'check_in_date': formatDate(start),
-            'check_out_date': formatDate(end)
-        })
-    }
-
-    const handleSubmitBooking = (e) => {
-        e.preventDefault()
-        postBooking(id, newBooking).then((response) => {
-            alert(response.data.msg)
-        })
-    }
+    const { bookedDates, handleChange, handleSubmitBooking } = useBooking()
 
     return (
         <div>

2e43ce55acee31f7c4961aac43e44eedf2b5b647
bric12369 <barryrichards05@gmail.com>
2025-09-10 13:52:09 +0100

    removed context

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -1,14 +1,11 @@
-import { useContext, useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { fetchProperties } from '../utils/apiCalls'
 import PropertyCard from './PropertyCard'
 import ToolBar from './ToolBar'
-import UserContext from '../Contexts/UserContext'
 
 const PropertyList = ({ properties, setProperties }) => {
 
     const [filters, setFilters] = useState({})
-
-    const user = useContext(UserContext)
 
     useEffect(() => {
         fetchProperties(filters).then((response) => {

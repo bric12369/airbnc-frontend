4eb651f6975ba848121ef4fc150f34f7402bf36a
bric12369 <barryrichards05@gmail.com>
2025-09-12 20:22:54 +0100

    added some styling

--- src/components/AnimatedButton.jsx
+++ src/components/AnimatedButton.jsx
@@ -3,7 +3,14 @@ import { motion } from "motion/react"
 
 const AnimatedButton = ({ text, onClick, disabled }) => {
 
-    return <motion.button whileHover={{ scale: 1.2 }} whileTap={{ scale: 0.8 }} onClick={onClick} disabled={disabled} >{text}</motion.button>
+    return (
+    <motion.button whileHover={{ scale: 1.2 }} 
+    whileTap={{ scale: 0.8 }} 
+    style={{ cursor: 'pointer'}}
+    onClick={onClick} 
+    className='button'
+    disabled={disabled}>{text}</motion.button>
+    )
 }
 
 export default AnimatedButton

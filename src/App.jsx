b4d2ed405077253793588495e03c7e6098c929b8
bric12369 <barryrichards05@gmail.com>
2025-09-12 18:00:59 +0100

    removed fragment

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ function App() {
 
   return (
     <UserContext value={userSignedIn}>
-      <>
         <Header users={users}/>
         <BrowserRouter>
           <Routes>
@@ -22,7 +21,6 @@ function App() {
             <Route path='/properties/:id' element={<PropertyDetailsPage />} />
           </Routes>
         </BrowserRouter>
-      </>
     </UserContext>
   )
 }

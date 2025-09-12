ad63da872e539676aaf823990bf2c8fc2c725260
bric12369 <barryrichards05@gmail.com>
2025-09-12 14:19:23 +0100

    Started login component and added event listener to profile-icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react"
+import Login from "./Login"
 
 const Header = () => {
+
+    const [profilePicClicked, setProfilePicClicked] = useState(false)
+
+    const handleClick = (e) => {
+        e.preventDefault()
+        setProfilePicClicked(!profilePicClicked)
+        console.log(profilePicClicked)
+    }
 
     return <div id='header'>
         <img src="/home.png" alt="home-icon" className="header-icon" />
         <h1>AirBnC</h1>
-        <img src="/avatar.png" alt="profile-icon" className="header-icon" />
+        <img src="/avatar.png" alt="profile-icon" className="header-icon" onClick={handleClick} />
+        { profilePicClicked && <Login /> }
     </div>
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react"
+
+const Login = () => {
+
+    return(
+        <div>
+            <p>Login section</p>
+        </div>
+    )
+}
+
+export default Login

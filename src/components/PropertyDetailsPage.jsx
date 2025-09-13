23e5581fbcb618737c78b38723aaab69d9e51b55
bric12369 <barryrichards05@gmail.com>
2025-09-13 14:42:02 +0100

    conditionally rendered reviews section

--- src/components/PropertyDetailsPage.jsx
+++ src/components/PropertyDetailsPage.jsx
@@ -24,29 +24,37 @@ const PropertyDetailsPage = () => {
             setShowReviewSection(true)
         }
     }
-    
-    if (isLoading || !property.images || !reviews.reviews) {
+
+    const stillLoading = isLoading || !property.images
+
+    if (stillLoading) {
         return <h3>Loading...</h3>
     } else {
-        return(
+        return (
             <div>
                 <h2>{property.property_name}</h2>
                 <p>{property.location}</p>
                 <div className="imageContainer">
-                {property.images.map((image, index) => {
-                    return <img key={image} className="propertyImage" src={image} alt={`${property.property_name} image ${index + 1}`} />
-                })}
+                    {property.images.map((image, index) => {
+                        return <img key={image} className="propertyImage" src={image} alt={`${property.property_name} image ${index + 1}`} />
+                    })}
                 </div>
                 <p>{property.description}</p>
                 <img src={property.host_avatar} alt={`Photo of host: ${property.host}`} id='hostImage' />
                 <figcaption>{`Host: ${property.host}`}</figcaption>
                 <NewBookingCard />
-                <h3 className="subheading">Reviews:</h3>
-                <p>{`Average rating: ${reviews.average_rating.toFixed(2)}`}</p>
-                {reviews.reviews.map((review) => {
-                    return <ReviewSnapshotCard key={review.review_id} review={review} />
-                })}
-                {showReviewSection === true ? <PropertyAddReviewSection setShowReviewSection={setShowReviewSection} /> : <AnimatedButton text={'Add review'} onClick={handleClick} /> }
+                {Array.isArray(reviews.reviews) && reviews.reviews.length > 0 ? (
+                    <>
+                        <h3 className="subheading">Reviews:</h3>
+                        <p>{`Average rating: ${reviews.average_rating.toFixed(2)}`}</p>
+                        {reviews.reviews.map((review) => {
+                            return <ReviewSnapshotCard key={review.review_id} review={review} />
+                        })}
+                    </>
+                ) : (
+                    <p>No reviews yet</p>
+                )}
+                {showReviewSection === true ? <PropertyAddReviewSection setShowReviewSection={setShowReviewSection} /> : <AnimatedButton text={'Add review'} onClick={handleClick} />}
             </div>
         )
     }

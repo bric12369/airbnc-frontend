84e2b278772d3eb2f44522077424ec4359f59ed3
bric12369 <barryrichards05@gmail.com>
2025-09-15 11:26:39 +0100

    added loading state

--- src/components/PropertyList.jsx
+++ src/components/PropertyList.jsx
@@ -4,11 +4,12 @@ import ToolBar from './ToolBar'
 
 const PropertyList = ({ properties, setProperties }) => {
 
-    const { setFilters } = useProperties(setProperties)
+    const { setFilters, isLoading } = useProperties(setProperties)
 
     return (
         <div id='propertyList'>
             <ToolBar setFilters={setFilters}/>
+            {isLoading && <p>Waking up the server...</p>}
             {properties.map((property) => {
                 return <PropertyCard property={property} key={property.property_id} showFavouriteButton={true} />
             })}
--- src/hooks/useProperties.js
+++ src/hooks/useProperties.js
@@ -3,12 +3,15 @@ import { fetchProperties } from '../utils/apiCalls'
 
 export default function useProperties( setProperties ) {
     const [filters, setFilters] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
+        setIsLoading(true)
         fetchProperties(filters).then((response) => {
             setProperties(response.data.properties)
+            setIsLoading(false)
         })
     }, [filters])
 
-    return { setFilters }
+    return { setFilters, isLoading }
 }

f68d33a2ea198a8338ace26dc587d07c4710bfc5
bric12369 <barryrichards05@gmail.com>
2025-09-17 20:32:00 +0100

    added bookings

--- src/components/HostPropertyDetailsPage.jsx
+++ src/components/HostPropertyDetailsPage.jsx
@@ -1,21 +1,53 @@
-import { useLocation, useNavigate } from "react-router"
 import usePropertyDetails from "../hooks/usePropertyDetails"
-import { useEffect } from "react"
+import useBooking from "../hooks/useBooking"
+import { useEffect, useState } from "react"
+import { fetchPropertyBookings } from "../utils/apiCalls"
+import { useParams } from "react-router"
+import { formatDateToDDMMYYYY } from "../utils/formatDates"
 
 const HostPropertyDetailsPage = () => {
 
     const { property, isLoading } = usePropertyDetails()
+    const [bookingsLoading, setBookingsLoading] = useState(true)
+    const [bookings, setBookings] = useState([])
+    const { id } = useParams()
 
-    if (isLoading || !property) {
+    useEffect(() => {
+        setBookingsLoading(true)
+        fetchPropertyBookings(id).then((response) => {
+            setBookings(response.data.bookings)
+            setBookingsLoading(false)
+        })
+    }, [])
+
+    if (isLoading || !property.images || bookingsLoading) {
         return <div>Loading...</div>;
+    } else {
+        return (
+            <div>
+                <h3>Host Property Details Page</h3>
+                <h2>{property.property_name}</h2>
+                <p>{property.location}</p>
+                <div className="imageContainer">
+                    {property.images.map((image, index) => {
+                        return <img key={image} className="propertyImage" src={image} alt={`${property.property_name} image ${index + 1}`} />
+                    })}
+                </div>
+                <p>{property.description}</p>
+                <h3>All Bookings:</h3>
+                {bookings.length > 0 ? (
+                    <div> 
+                        {bookings.map((booking) => {
+                        return <p key={booking.booking_id}>{`${formatDateToDDMMYYYY(booking.check_in_date.split('T')[0])} - ${formatDateToDDMMYYYY(booking.check_out_date.split('T')[0])}`}</p>
+                    })}
+                    </div>
+                ) :
+                    <p>No bookings yet</p>
+                }
+            </div>
+        )
     }
 
-    return (
-        <div>
-            <h3>Host Property Details Page</h3>
-            <p>{property.property_name}</p>
-        </div>
-    )
 }
 
 export default HostPropertyDetailsPage

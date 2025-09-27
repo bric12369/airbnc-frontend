8838226141a641531fbfe3a886ed3138e40ba208
bric12369 <barryrichards05@gmail.com>
2025-09-27 16:19:37 +0100

    compared id param to id signed in to ensure user is navigated to correct profile page path

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -2,8 +2,19 @@ import ProfileDetails from "./ProfileDetails"
 import BookingsList from "./BookingsList"
 import HostPropertyList from "./HostPropertyList"
 import Loading from "./Loading"
+import { Navigate, useParams } from "react-router"
+import { useContext } from "react"
+import UserContext from "../Contexts/UserContext"
 
 const ProfilePage = ({ bookings, setBookings, profile, setProfile, error, isLoading, properties }) => {
+
+    const { id } = useParams()
+    const { userIdSignedIn } = useContext(UserContext)
+
+
+    if (id !== userIdSignedIn) {
+        return <Navigate to={`/user/${userIdSignedIn}/profile`} replace />
+    }
 
     if (error) {
         return <p>Please login to view your profile</p>
@@ -16,8 +27,8 @@ const ProfilePage = ({ bookings, setBookings, profile, setProfile, error, isLoad
                 <div className="flexContainer">
                     <h2>{`Hello, ${profile.first_name}!`}</h2>
                     <ProfileDetails profile={profile} setProfile={setProfile} />
-                    { profile.is_host === true && <HostPropertyList profile={profile} properties={properties}/> }
-                    <BookingsList bookings={bookings} setBookings={setBookings}/>
+                    {profile.is_host === true && <HostPropertyList profile={profile} properties={properties} />}
+                    <BookingsList bookings={bookings} setBookings={setBookings} />
                 </div>
             )
         }

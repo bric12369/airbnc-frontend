f63d0916ae9a36e3ef10a0fc9cf7b48e9aed471e
bric12369 <barryrichards05@gmail.com>
2025-09-27 14:57:18 +0100

    added error state

--- src/components/HostPropertyListings.jsx
+++ src/components/HostPropertyListings.jsx
@@ -2,27 +2,37 @@ import { useEffect, useState } from "react"
 import { fetchProperties } from "../utils/apiCalls"
 import { useParams } from "react-router"
 import PropertyCard from "./PropertyCard"
+import Loading from "./Loading"
 
 const HostPropertyListings = () => {
 
     const {id} = useParams()
 
     const [properties, setProperties] = useState([])
-
+    const [isLoading, setIsLoading] = useState(true)
+    
     useEffect(() => {
-        console.log(id)
+        setIsLoading(true)
         fetchProperties({'host_id': id}).then((response) => {
             setProperties(response.data.properties)
+            setIsLoading(false)
         })
     }, [id])
-
-    return(
-        <div className="flexContainer" id="hostListingsContainer">
-            {properties.map((property) => {
-                return <PropertyCard key={property.property_id} property={property} />
-            })}
-        </div>
-    )
+    
+    if (isLoading) {
+        return <Loading />
+    } else {
+        return(
+            <>
+            <h2>{properties[0].host}'s Listings:</h2>
+                <div className="flexContainer" id="hostListingsContainer">
+                    {properties.map((property) => {
+                        return <PropertyCard key={property.property_id} property={property} />
+                    })}
+                </div>
+            </>
+        )
+    }
 }
 
 export default HostPropertyListings
